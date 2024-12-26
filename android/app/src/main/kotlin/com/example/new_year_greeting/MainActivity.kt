df73f2c7975d3cb61cfd34a7cefd0f59f856fd6b
Shakirullah <omotososakiru25@gmail.com>
2024-12-26 16:42:49 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/new_year_greeting/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.new_year_greeting
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/new_yr_greeting.dart
@@ -0,0 +1,203 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:confetti/confetti.dart';
+import 'package:flutter/material.dart';
+// import 'package:lottie/lottie.dart';
+
+class NewYrGreetings extends StatefulWidget {
+  final String title;
+
+  const NewYrGreetings({
+    this.title = "F E S T I V E  G R E E T I N G S",
+    super.key,
+  });
+
+  @override
+  State<NewYrGreetings> createState() => _NewYrGreetingsState();
+}
+
+class _NewYrGreetingsState extends State<NewYrGreetings> {
+  late final ConfettiController confettiController;
+  late final AudioPlayer audioPlayer;
+
+  void _playCelebratoryMusic() async {
+    try {
+      await audioPlayer.play(
+        AssetSource(
+            "lib/asset/sound/happy-mood-and-ukulele-version-60s-9895.mp3"),
+      );
+      print("Music is playing!");
+    } catch (e) {
+      print("error playing the music: $e");
+    }
+  }
+
+  @override
+  void initState() {
+    confettiController = ConfettiController(duration: Duration(seconds: 30));
+    confettiController.play();
+    audioPlayer = AudioPlayer();
+    _playCelebratoryMusic();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    audioPlayer.dispose();
+    confettiController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // implementing animated text in the appbar that fades in and out with sparkle effect
+      appBar: AppBar(
+        elevation: 0.0,
+        title: Column(
+          children: [
+            AnimatedTextKit(
+              repeatForever: true,
+              animatedTexts: [
+                FadeAnimatedText(
+                  widget.title,
+                  textStyle: TextStyle(
+                    color: Colors.white,
+                    fontFamily: "Rubik",
+                    fontSize: 20.0,
+                  ),
+                ),
+              ],
+            ),
+            AnimatedTextKit(
+              repeatForever: true,
+              animatedTexts: [
+                FadeAnimatedText(
+                  "F R O M  S H A K I R U L L A H",
+                  textStyle: TextStyle(
+                    color: Colors.white,
+                    fontFamily: "Rubik",
+                    fontSize: 15.0,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        toolbarHeight: 80,
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+
+      body: Center(
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.red,
+                    Color(int.parse('0xFFFFD700')),
+                  ],
+                ),
+              ),
+            ),
+            Center(
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  final size = constraints.maxWidth > 600
+                      ? constraints.maxWidth * 0.04
+                      : constraints.maxWidth * 0.04;
+                  return Text(
+                    "Happy New Year!",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: size,
+                      fontFamily: "Rubik",
+                      fontWeight: FontWeight.bold,
+                    ),
+                  );
+                },
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  final size = constraints.maxWidth > 600
+                      ? constraints.maxWidth * 0.04
+                      : constraints.maxWidth * 0.04;
+                  return Padding(
+                    padding: constraints.maxWidth > 600
+                        ? const EdgeInsets.only(top: 150.0)
+                        : const EdgeInsets.only(top: 100.0),
+                    child: Text(
+                      "Cheers to new beginnings!",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: size,
+                        fontFamily: "Rubik",
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  final size = constraints.maxWidth > 600
+                      ? constraints.maxWidth * 0.04
+                      : constraints.maxWidth * 0.03;
+                  return Padding(
+                    padding: constraints.maxWidth > 600
+                        ? const EdgeInsets.only(top: 300.0)
+                        : const EdgeInsets.only(top: 200.0),
+                    child: AnimatedTextKit(
+                      animatedTexts: [
+                        TyperAnimatedText(
+                          "This year, aim higher and shine brighter!",
+                          textStyle: TextStyle(
+                            color: Colors.white,
+                            fontSize: size,
+                            fontFamily: "Rubik",
+                          ),
+                          speed: const Duration(milliseconds: 100),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: ConfettiWidget(
+                confettiController: confettiController,
+                blastDirectionality: BlastDirectionality.explosive,
+                emissionFrequency: 0.05,
+                shouldLoop: true,
+                numberOfParticles: 80,
+                maxBlastForce: 10,
+                minBlastForce: 5,
+                gravity: 0.3,
+                colors: [
+                  Colors.black,
+                  Colors.white,
+                  Colors.red,
+                  Color(int.parse('0xFFFFD700')),
+                  Colors.purple,
+                  Colors.green,
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
