46d5e0d704835589fe6d48cdd436805e8fd89ca1
Shakirullah <omotososakiru25@gmail.com>
2024-12-27 06:34:34 +0100

    Changes commited

--- lib/new_yr_greeting.dart
+++ lib/new_yr_greeting.dart
@@ -2,7 +2,6 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
-// import 'package:lottie/lottie.dart';
 
 class NewYrGreetings extends StatefulWidget {
   final String title;
